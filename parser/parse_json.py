80da3c4e9cc2e8f4b43f3c1ec4e53c66c16a1695
Hacko Evgeniy <e.hacko.nicecode@gmail.com>
2017-01-16 18:04:14 +0300

    Task 3 - add tag classes

--- parser/parse_json.py
+++ parser/parse_json.py
@@ -6,22 +6,65 @@ import sys
 SOURCE_JSON = 'source.json'
 
 
-def render_tag(tag, content):
-    """
-    Wrap content by html tags.
-    """
-    return '<{0}>{1}</{0}>'.format(tag, content)
+class BaseTag(object):
+
+    def __init__(self, parent=None):
+        if parent:
+            parent.add(self)
+        self.tag_name = None
+        self.children = []
+
+    def render(self):
+        raise NotImplementedError
+
+
+class Tag(BaseTag):
+
+    FORMAT_STR = '<{0}>{1}</{0}>'
+
+    def __init__(self, tag_name=None, parent=None):
+        super(Tag, self).__init__(parent)
+        self.tag_name = tag_name
+
+    def add(self, *tags):
+        self.children.extend(tags)
+
+    def render(self):
+        children = ''.join([child.render() for child in self.children])
+        if self.tag_name:
+            return self.FORMAT_STR.format(self.tag_name, children)
+        return children
+
+    def bind_obvious_tags(self, **kwargs):
+        for key, value in kwargs.items():
+            tag = Tag(key)
+            tag.add(TextTag(value))
+            self.add(tag)
+
+
+class TextTag(BaseTag):
+
+    def __init__(self, text='', parent=None):
+        super(TextTag, self).__init__(parent)
+        self.text = text
+
+    def render(self):
+        return self.text
 
 
 def main():
     with open(SOURCE_JSON) as f:
         data = json.load(f, object_pairs_hook=OrderedDict)
+    if not isinstance(data, list):
+        root = Tag()
+        root.bind_obvious_tags(**data)
+    else:
+        root = Tag('ul')
+        for el in data:
+            li = Tag('li', root)
+            li.bind_obvious_tags(**el)
 
-    result = []
-    for el in data:
-        for key, value in el.items():
-            result.append(render_tag(key, value))
-    sys.stdout.write("".join(result))
+    sys.stdout.write(root.render())
 
 
 if __name__ == "__main__":
--- parser/tests.py
+++ parser/tests.py
@@ -1,5 +1,5 @@
 import unittest
-from parse_json import render_tag
+from parse_json import Tag, TextTag
 
 
 class TestTags(unittest.TestCase):
@@ -13,14 +13,40 @@ class TestTags(unittest.TestCase):
         self.test_value2 = 'test2'
         self.test_result2 = '<p>test2</p>'
 
+    def test_text_tag(self):
+        """
+        Test `TextTag` functionality
+        """
+        content = 'Hello world!'
+        self.assertEqual(TextTag(content).render(), content)
+
+        content = 'Good day!'
+        self.assertEqual(TextTag(content).render(), content)
+
     def test_render_tag(self):
         """
-        Test `render_tag` functionality
+        Test `Tag` functionality
         """
-        self.assertEqual(render_tag(self.test_tag1, self.test_value1),
-                         self.test_result1)
-        self.assertEqual(render_tag(self.test_tag2, self.test_value2),
-                         self.test_result2)
+        tag1 = Tag(self.test_tag1)
+        tag1.add(TextTag(self.test_value1))
+        self.assertEqual(tag1.render(), self.test_result1)
+
+        tag2 = Tag(self.test_tag2)
+        tag2.add(TextTag(self.test_value2))
+        self.assertEqual(tag2.render(), self.test_result2)
+
+    def test_children(self):
+        """
+        Test `children` functionality
+        """
+        tag1 = Tag(self.test_tag1)
+        tag1.add(TextTag(self.test_value1))
+
+        tag2 = Tag(self.test_tag2, tag1)
+        tag2.add(TextTag(self.test_value2))
+
+        html = '<h1>test1<p>test2</p></h1>'
+        self.assertEqual(tag1.render(), html)
 
 
 if __name__ == '__main__':
